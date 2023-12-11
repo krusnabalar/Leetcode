f37cb6e41a7d38ee5e174d75e9f735ef1862f957
krusnabalar <90931286+krusnabalar@users.noreply.github.com>
2023-12-11 18:13:06 +0530

    rewrote 2.java

--- java/0002.java
+++ java/0002.java
@@ -1,33 +1,31 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(0);
-        ListNode curr = res;
-        int sum = 0;
+        ListNode sum = new ListNode(0);
+        ListNode res = sum;
         int carry = 0;
-        while (l1 != null || l2 != null || carry == 1) {
+        while (l1 != null || l2 != null || carry != 0) {
+            int value = carry;
             if (l1 != null) {
-                sum += l1.val;
+                value += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
-                sum += l2.val;
+                value += l2.val;
                 l2 = l2.next;
             }
-            sum += carry;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            sum = 0;
+            sum.next = new ListNode(value % 10);
+            carry = value / 10;
+            sum = sum.next;
         }
 
         return res.next;
